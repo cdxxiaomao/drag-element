9233151cded0e67b275cbff33315ffec8d2df6d1
chendeng <530972294@qq.com>
2025-03-10 00:51:48 +0800

    test: 拖拽测试用例

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="container">
-    Vue test
+    <div
+      id="draggable"
+      style="width: 100px; height: 100px; border: 1px solid #ccc; box-sizing: border-box; border-radius: 20px; overflow:hidden;"
+    >
+      <div
+        id="handle"
+        class="dragEl"
+      >
+        拖拽点
+      </div>
+      <div
+        id="handle1"
+        class="dragEl"
+      >
+        拖拽点1
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { dragElement } from '../lib'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  dragElement('#draggable', {
+    reference: 'window',
+    // reference: 'parent',
+    handle: ['#handle', '#handle1']
+    // useVirtual: true
+  })
+})
 
 </script>
 
 <style scoped lang="less">
 .container {
-  width: 100%;
-  height: 100%
+  font-size: 12px;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #c4c4c4;
+  position:relative;
+  margin: 30px;
+  overflow: auto;
+  border-radius: 20px;
+}
+
+.dragEl {
+  width: 60px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 50px;
+  margin-top: 20px;
+  cursor: pointer;
+  &:hover {
+    background: #d9d9d9;
+  }
 }
 </style>
